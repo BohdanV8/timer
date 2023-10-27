888974edcc1384bf06f1f80428613af3a5b5d82a
BohdanV8 <thorykbv@gmail.com>
2023-10-27 23:55:29 +0300

    My timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap";
+import "./styles/app.css";
+import MyTimer from "./components/MyTimer";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center mt-5">
+      <h1 className="RacingSansOne">Timer</h1>
+      <MyTimer />
     </div>
   );
 }
